7146ba36e31427dd88a709b9ff98b2db3268513d
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2023-12-13 18:35:40 +0100

    refacorting of some headers and used [[nodiscard]]

--- src/dersbiander/TokenizerUtils.cpp
+++ src/dersbiander/TokenizerUtils.cpp
@@ -1,6 +1,9 @@
 #include "TokenizerUtils.h"
 #include <unordered_set>
 
+DISABLE_WARNINGS_PUSH(
+    4005 4201 4459 4514 4625 4626 4820 6244 6285 6385 6386 26409 26415 26418 26429 26432 26437 26438 26440 26446 26447 26450 26451 26455 26457 26459 26460 26461 26467 26472 26473 26474 26475 26481 26482 26485 26490 26491 26493 26494 26495 26496 26497 26498 26800 26814 26818 26826)
+
 bool TokenizerUtils::isPlusORMinus(char c) noexcept { return c == '+' || c == '-'; }
 
 bool TokenizerUtils::isComment(const std::span<const char> &inputSpan, size_t position) noexcept {
@@ -33,3 +36,5 @@ bool TokenizerUtils::isVarLenOperator(const std::string &val) noexcept {
 bool TokenizerUtils::isBrackets(char c) noexcept { return c == '(' || c == ')' || c == '[' || c == ']' || c == '{' || c == '}'; }
 
 bool TokenizerUtils::isApostrophe(char c) noexcept { return c == '\''; }
+
+DISABLE_WARNINGS_POP()
--- src/dersbiander/TokenizerUtils.h
+++ src/dersbiander/TokenizerUtils.h
@@ -1,18 +1,17 @@
 #pragma once
+#include "headers.h"
 #include <span>
-#include <string>
-#include <vector>
 
 class TokenizerUtils {
 public:
-    static bool isPlusORMinus(char c) noexcept;
-    static bool isComment(const std::span<const char> &inputSpan, size_t position) noexcept;
-    static bool isOperator(char c) noexcept;
-    static bool isOperationEqualOperator(const std::string &value) noexcept;
-    static bool isBooleanOperator(const std::string &value) noexcept;
-    static bool isLogicalOperator(const std::string &value) noexcept;
-    static bool isUnaryOperator(const std::string &value) noexcept;
-    static bool isVarLenOperator(const std::string &val) noexcept;
-    static bool isBrackets(char c) noexcept;
-    static bool isApostrophe(char c) noexcept;
+    [[nodiscard]] static bool isPlusORMinus(char c) noexcept;
+    [[nodiscard]] static bool isComment(const std::span<const char> &inputSpan, size_t position) noexcept;
+    [[nodiscard]] static bool isOperator(char c) noexcept;
+    [[nodiscard]] static bool isOperationEqualOperator(const std::string &value) noexcept;
+    [[nodiscard]] static bool isBooleanOperator(const std::string &value) noexcept;
+    [[nodiscard]] static bool isLogicalOperator(const std::string &value) noexcept;
+    [[nodiscard]] static bool isUnaryOperator(const std::string &value) noexcept;
+    [[nodiscard]] static bool isVarLenOperator(const std::string &val) noexcept;
+    [[nodiscard]] static bool isBrackets(char c) noexcept;
+    [[nodiscard]] static bool isApostrophe(char c) noexcept;
 };
